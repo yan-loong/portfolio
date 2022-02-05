1eb02a48beed15d8982bdc77becf3bb358461bd4
yan-loong <guramivarberidze@gmail.com>
2022-02-05 18:47:59 +0400

    add ui and util  lib to project

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,6 +11,11 @@ app.get('/api/test', (req, res) => {
   res.send({ message: 'Welcome to api! GURO' });
 });
 
+
+app.get('/test', (req, res) => {
+  res.send({ message: 'Welcome to api! GURO' });
+});
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
--- apps/task-management/src/app/app.component.ts
+++ apps/task-management/src/app/app.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor(private http: HttpClient) {
-    this.http.get('api/test').subscribe(console.log)
+    this.http.get('/test').subscribe(console.log)
   }
 }
